8205669ea8d755d6213fe6ddc534123be4390891
GodwinSaji9 <sajigodwin44@gmail.com>
2026-03-09 10:44:17 +0530

    Godwin Added feature/UC1-WelcomePage

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,6 +1,6 @@
 public class BookMyStayApp {
     public static void main(String[]args){
-        System.out.println("Welcome to the hotel Booking Management System");
-        System.out.println("System Initialized Successfully");
+        System.out.println("Welcome to the hotel booking Management System");
+        System.out.println("System Initialized successfully");
     }
 }
