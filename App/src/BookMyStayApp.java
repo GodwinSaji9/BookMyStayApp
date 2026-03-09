2113da0699060147c8b442669f964836369c4dd3
GodwinSaji9 <sajigodwin44@gmail.com>
2026-03-09 10:33:16 +0530

    UseCase1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class BookMyStayApp {
+    public static void main(String[]args){
+        System.out.println("Welcome to the hotel Booking Management System");
+        System.out.println("System Initialized Successfully");
+    }
+}
